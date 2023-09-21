88995d4814930ee19adaedd854809fc7f39b3f86
Nicole Law <nicole.law12@gmail.com>
2023-09-21 17:32:14 -0400

    registration routes

--- backend/schema.sql
+++ backend/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE users (
   id SERIAL PRIMARY KEY, 
   name VARCHAR(255) NOT NULL, 
-  email VARCHAR(255) NOT NULL, 
+  email VARCHAR(255) NOT NULL UNIQUE, 
   password VARCHAR(50) NOT NULL 
 );
 
--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,51 @@
-const express = require('express');
-const db = require('db');
+// Import the Express.js library and the 'db' module
+const express = require('express'); // used to create web apps in Node.js 
+const db = require('./db'); // connecting to PostgreSQL db
+const bcrypt = require('bcrypt');
 
-const app = express();
+// Create an instance of the Express app
+const app = express(); // app obj will be used to define routes and configure web server 
+
+// Use the JSON middleware to parse JSON request bodies
+app.use(express.json());
+
+// Route that gets the root URL
+app.get('/', (req, res) => {
+  res.send('The To Do List is running!');
+});
+
+// Route that gets the register page
+app.get('/register', (req, res) => {
+  res.send('Register page running');
+});
+
+// Route that gets the login page
+app.get('/login', (req, res) => {
+  res.send('Login page running');
+});
+
+// Route that creates a new user
+app.post('/register', async(req, res)=> {
+  const {name, email, password } = req.body;
+  if (!name || !email || !password ) {
+    return res.status(400).json({error: 'All fields are required'});
+  } 
+  try {
+    // Hash the user's password 
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await db.query('INSERT INTO users (name, email, password) VALUES ($1, $2, $3)', [name, email, hashedPassword])
+    return res.status(200).json({ message: 'User registration successfull'});
+  } catch(err) {
+    console.log(err);
+    return res.status(500).json({ error: 'An error occured...'});
+  }
+});
+
+// Set the server's listening port to 3000
+const PORT = 3000;
+
+// Start the Express server and listen on the specified port 
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
