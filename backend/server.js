9232cd2f2b9e14b6ee49dd6250713d0fd06ade76
Nicole Law <nicole.law12@gmail.com>
2023-09-28 18:08:26 -0400

    finished routes for to do list

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 // Import the Express.js library and the 'db' module
 const express = require('express'); // used to create web apps in Node.js 
 const db = require('./db'); // connecting to PostgreSQL db
-const bcrypt = require('bcrypt');
-const cors = require('cors');
+const bcrypt = require('bcrypt'); // for passwords
+const cors = require('cors'); // allows access from front to backend 
 const { getUserByEmail, comparePasswords } = require('./dbUtils');
 
 // Create an instance of the Express app
@@ -11,10 +11,10 @@ const app = express(); // app obj will be used to define routes and configure we
 // Use the JSON middleware to parse JSON request bodies
 app.use(express.json());
 
-// Enables CORS for specific originals 
+// Enables CORS for specific origins
 app.use(cors({ origin: 'http://localhost:3000'}));
 
-// Route that gets the root URL
+// Route that gets the root URL for testing purposes
 app.get('/', (req, res) => {
   res.send('The To Do List is running!');
 });
@@ -68,6 +68,97 @@ app.post('/login', async(req, res) => {
     return res.status(500).json({error: 'An error occured...', errorMessage: err.message});
   }
 })
+
+// Route to get existing to do list items from logged in user 
+app.get('/todos/:userId', async(req, res) => {
+  const userId = req.params.userId; 
+
+  try {
+    const result = await db.query('SELECT * FROM tasks WHERE user_id = $1', [userId]);
+    //accessing all of the tasks, comes back as an array
+    const tasks = result.rows;
+
+    return res.status(200).json({tasks});
+
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ error: 'An error occured while fetching your to do list.'});
+  }
+})
+
+
+// Route to create a new to do list item 
+app.post('/todos', async(req, res) => {
+
+  // coming in from the front end
+  const {userId, task, status, dueDate, priority } = req.body;
+
+  if(!task) {
+    return res.status(400).json({error: 'Task is required'});
+  }
+
+  try {
+    const result = await db.query('INSERT INTO tasks (user_id, task, status, due_date, priority) VALUES ($1, $2) RETURNING *', [userId, task, status, dueDate, priority]);
+    const newToDo = result.rows[0];
+    return res.send(200).json({message: 'To Do item successfully created', todo: newToDo});
+    
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({error: 'An error occured while creating task.'})
+  }
+})
+
+
+// Route to update a to do list item 
+app.put('/todos/:taskId', async(req, res) => {
+  const taskId = req.params.taskId;
+
+  if(!taskId) {
+    return res.status(400).json({error: 'Task ID required'});
+  }
+
+  const { userId, task, status, dueDate, priority } = req.body;
+
+  try {
+    const result = await db.query(
+      'UPDATE tasks SET user_id = $1 task = $2, status = $3, due_date = $4, priority = $5 WHERE id = $6', [userId, task, status, dueDate, priority, taskId]
+    );
+    
+    if (result.rowCount === 1) {
+      return res.status(200).json({message: 'Task successfully updated', task: result.rows[0]});
+    } else {
+      return res.status(404).json({error: 'Task not found'});
+    }
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({error: 'An error occured while updating task'});
+  }
+})
+
+
+// Route to delete a to do list item 
+app.delete('/todos/:taskId', async(req, res) => {
+  const taskId = req.params.taskId; 
+
+  if (!taskId) {
+    return res.status(400).json({error: 'Task ID required'});
+  }
+
+  try {
+    const result = await db.query(
+      'DELETE FROM tasks WHERE id = $1', [taskId]
+    );
+    if (result.rowCount === 1 ){
+      return res.status(200).json({message: 'Task successfully deleted'})
+    } else {
+      return res.status(404).json({ error: 'Task not found'})
+    }
+  } catch(err) {
+    console.log(err);
+    return res.status(500).json({error: 'An error occured while deleting task'});
+  }
+})
+
 
 // Set the server's listening port 
 const PORT = 8080;
